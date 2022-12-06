88e62b801a76e0d237ebc8226b0eee9dae67e502
Jeliiiii <elacroix@gaming.tech>
2022-12-06 19:41:04 +0100

    maj

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require("./db/db");
 const jsonParser = bodyParser.json();
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dbo.connectToServer();
 app.get("/", function (req, res) {
@@ -34,8 +35,6 @@ app.get("/pokemon/get", function (req, res) {
         });
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.post('/pokemon/post', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
     const body = req.body;
@@ -60,6 +59,58 @@ app.delete('/pokemon/delete', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
     const body = req.body;
     dbConnect.collection("Pokemon").deleteOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+
+
+app.get("/pokemon/get", function (req, res) {
+    //on se connecte à la DB MongoDB
+    const dbConnect = dbo.getDb();
+    //premier test permettant de récupérer mes pokemons !
+    dbConnect
+        .collection("Pokedex")
+        .find({}) // permet de filtrer les résultats
+        /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+        .toArray(function (err, result) {
+            if (err) {
+                res.status(400).send("Error fetching pokemons!");
+            } else {
+                res.json(result);
+            }
+        });
+});
+
+
+app.post('/pokemon/post', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    dbConnect.collection("Pokedex").insertOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    console.log('Got body:', body);
+    dbConnect.collection("Pokedex").updateOne({num:body.num}, {$set:{num:body.newnum , name:body.newname , type:body.newtype}})
+    res.json(body);
+});
+
+app.delete('/pokemon/delete', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    dbConnect.collection("Pokedex").deleteOne(body).then(function (result, err) {
         if (err) {
             res.status(400).send(err.message);
         } else {

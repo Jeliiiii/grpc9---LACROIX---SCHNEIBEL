1da4da4a841c90a2b576685a523b99023c73aabf
Jeliiiii <elacroix@gaming.tech>
2022-12-06 18:43:21 +0100

    maj

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dbo = require("./db/db");
 const app = express();
 const port = 4444;
 const bodyParser = require('body-parser');
+const db = require("./db/db");
 const jsonParser = bodyParser.json();
 
 
@@ -16,7 +17,7 @@ app.listen(port, function () {
     console.log(`Vous êtes sur le port: ${port}!`);
 });
 
-app.get("/pokemon5g/Pokemon", function (req, res) {
+app.get("/pokemon/get", function (req, res) {
     //on se connecte à la DB MongoDB
     const dbConnect = dbo.getDb();
     //premier test permettant de récupérer mes pokemons !
@@ -34,18 +35,36 @@ app.get("/pokemon5g/Pokemon", function (req, res) {
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/pokemon5g/Pokemon', jsonParser, (req, res) => {
+
+app.post('/pokemon/post', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
-    dbConnect
+    const body = req.body;
+    dbConnect.collection("Pokemon").insertOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
     const body = req.body;
     console.log('Got body:', body);
+    dbConnect.collection("Pokemon").updateOne({name:body.name}, {$set:{name:body.to}})
+    dbConnect.collection("Pokemon").updateOne({type:body.type}, {$set:{type:body.maj}})
     res.json(body);
 });
 
-app.delete('/pokemon5g/Pokemon', jsonParser, (req, res) => {
+app.delete('/pokemon/delete', jsonParser, (req, res) => {
     const dbConnect = dbo.getDb();
-    dbConnect
     const body = req.body;
-    console.log('Got body:', body);
-    res.json(body);
+    dbConnect.collection("Pokemon").deleteOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
 });

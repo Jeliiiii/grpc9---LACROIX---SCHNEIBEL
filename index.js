530faa04adee006f83d539521a6c71d8a8ebaeaa
Jeliiiii <elacroix@gaming.tech>
2022-12-07 08:44:16 +0100

    maj

--- index.js
+++ index.js
@@ -67,7 +67,54 @@ app.delete('/pokemon/delete', jsonParser, (req, res) => {
     });
 });
 
+app.get("/pokemon/get", function (req, res) {
+    //on se connecte à la DB MongoDB
+    const dbConnect = dbo.getDb();
+    //premier test permettant de récupérer mes pokemons !
+    dbConnect
+        .collection("TypePokemon")
+        .find({}) // permet de filtrer les résultats
+        /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+        .toArray(function (err, result) {
+            if (err) {
+                res.status(400).send("Error fetching pokemons!");
+            } else {
+                res.json(result);
+            }
+        });
+});
 
+app.post('/pokemon/post', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    dbConnect.collection("TypePokemon").insertOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    console.log('Got body:', body);
+    dbConnect.collection("TypesPokemon").updateOne({type:body.type}, {$set:{type:body.newtype}})
+    res.json(body);
+});
+
+app.delete('/pokemon/delete', jsonParser, (req, res) => {
+    const dbConnect = dbo.getDb();
+    const body = req.body;
+    dbConnect.collection("TypePokemon").deleteOne(body).then(function (result, err) {
+        if (err) {
+            res.status(400).send(err.message);
+        } else {
+            res.json(result);
+        }
+    });
+});
 
 app.get("/pokemon/get", function (req, res) {
     //on se connecte à la DB MongoDB
